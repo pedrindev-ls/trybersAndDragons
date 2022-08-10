ec8faeb589192e164cbf9b691f0aee722a80e4c8
Pedrin <pedro515vieira@gmail.com>
2022-08-10 20:38:55 -0300

    :heavy_check_mark: feat: Requisito 6, Criação da interface FIghter com os atributos e metodos correspondentes

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,11 @@
+export default interface Fighter {
+  lifePoints:number
+  strength:number
+  defense:number
+  energy?:number
+
+  attack(enemy:Fighter):void
+  special?(enemy:Fighter):void
+  levelUp():void
+  receiveDamage(attackPoints:number):number
+}
